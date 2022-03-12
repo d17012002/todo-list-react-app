3aa5fcdc0fcef9987e03fd3041368c88eff1df55
Anurag Kumar <2002anuragksingh@gmail.com>
2022-03-12 21:39:46 +0530

    React App

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [inputItem, setInputItem] = useState("");
+  const [inputArray, setinputArray] = useState([]);
+  function handleChange(event){
+    setInputItem(event.target.value);
+  }
+  function addItem(){
+    setinputArray(prevValue =>{
+      return [...prevValue, inputItem];
+    });
+    setInputItem("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputItem} />
+        <button>
+          <span onClick={addItem}>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {inputArray.map((arrayItem) => {
+            return  <li>{arrayItem}</li>})} 
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
